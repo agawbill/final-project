34118bc29fb780f426fc2916fad97a9ceae32fb6
Anthony Gawbill <gaw316@gmail.com>
2018-07-03 15:04:54 -0400

    seach is fully functional, i just need to add pages

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,10 +1,7 @@
 class SearchesController < ApplicationController
 
-  # include HTTParty
-  # base_uri 'api.themoviedb.org/3/movie/550?api_key=720b022cdc841c69c719e5c1b854fd67'
 
   def index
-    @genreList=Tmdb::Genre.movie_list
     if params[:d].present? && params[:genresD].present? && params[:ratingD].present?
       @movies=Tmdb::Search.movie(params[:d])
       solid=@movies.results.find_all{|favor| favor.genre_ids.include? params[:genresD].to_i }
@@ -16,6 +13,22 @@ class SearchesController < ApplicationController
     elsif params[:l].present? && params[:rating].present?
       @genres=Tmdb::Genre.movies(params[:l])
       @results=@genres.results.find_all { |favor| favor.vote_average >= params[:rating].to_i}
+    elsif params[:genresP].present? && params[:ratingP].present?
+      @movies=Tmdb::Movie.popular.results
+      @solid=@movies.find_all { |favor| favor.genre_ids.include? params[:genresP].to_i}
+      @results=@solid.find_all { |favor| favor.vote_average >= params[:ratingP].to_i }
+    elsif params[:genresT].present? && params[:ratingT].present?
+      @movies=Tmdb::Movie.top_rated.results
+      @solid=@movies.find_all { |favor| favor.genre_ids.include? params[:genresT].to_i}
+      @results=@solid.find_all { |favor| favor.vote_average >= params[:ratingT].to_i }
+    elsif params[:genresU].present? && params[:ratingU].present?
+      @movies=Tmdb::Movie.upcoming.results
+      @solid=@movies.find_all { |favor| favor.genre_ids.include? params[:genresU].to_i}
+      @results=@solid.find_all { |favor| favor.vote_average >= params[:ratingU].to_i }
+    elsif params[:genresL].present? && params[:ratingL].present?
+      @movies=Tmdb::Movie.now_playing.results
+      @solid=@movies.find_all { |favor| favor.genre_ids.include? params[:genresL].to_i}
+      @results=@solid.find_all { |favor| favor.vote_average >= params[:ratingL].to_i }
     elsif params[:q].present? && params[:genres].present?
       @movies=Tmdb::Search.movie(params[:q])
       @results=@movies.results.find_all { |favor| favor.genre_ids.include? params[:genres].to_i }
@@ -28,6 +41,32 @@ class SearchesController < ApplicationController
     elsif params[:d].present? && params[:ratingD].present?
       @movies=Tmdb::Search.person(params[:d])
       @results=@movies.results[0].known_for.find_all { |favor| favor.vote_average >= params[:ratingD].to_i }
+    elsif params[:genresP].present?
+      @movies=Tmdb::Movie.popular.results
+      @results=@movies.find_all { |favor| favor.genre_ids.include? params[:genresP].to_i}
+    elsif params[:genresT].present?
+      @movies=Tmdb::Movie.top_rated.results
+      @results=@movies.find_all { |favor| favor.genre_ids.include? params[:genresT].to_i}
+    elsif params[:genresU].present?
+      @movies=Tmdb::Movie.upcoming.results
+      @results=@movies.find_all { |favor| favor.genre_ids.include? params[:genresU].to_i}
+    elsif params[:genresL].present?
+      @movies=Tmdb::Movie.now_playing.results
+      @results=@movies.find_all { |favor| favor.genre_ids.include? params[:genresL].to_i}
+    elsif params[:ratingP].present?
+      @movies=Tmdb::Movie.popular.results
+      @results=@movies.find_all { |favor| favor.vote_average >= params[:ratingP].to_i }
+    elsif params[:ratingU].present?
+      @movies=Tmdb::Movie.upcoming.results
+      @results=@movies.find_all { |favor| favor.vote_average >= params[:ratingU].to_i }
+    elsif params[:ratingL].present?
+      @movies=Tmdb::Movie.now_playing.results
+      @results=@movies.find_all { |favor| favor.vote_average >= params[:ratingL].to_i }
+    elsif params[:ratingT].present?
+      @movies=Tmdb::Movie.top_rated.results
+      @results=@movies.find_all { |favor| favor.vote_average >= params[:ratingT].to_i }
+    elsif params[:allPop].present?
+      @results=Tmdb::Movie.popular.results
     elsif params[:d].present?
       @movies=Tmdb::Search.person(params[:d])
       @results=@movies.results[0].known_for
@@ -43,6 +82,11 @@ class SearchesController < ApplicationController
    else
      @movies=[]
      @results=[]
+     @popularMovies=Tmdb::Movie.popular.results
+     @topRatedMovies=Tmdb::Movie.top_rated.results
+     @topRatedMovies=Tmdb::Movie.top_rated.results
+     @nowPlayingMovies=Tmdb::Movie.now_playing.results
+     @upcomingMovies=Tmdb::Movie.upcoming.results
    # end
  end
   end
