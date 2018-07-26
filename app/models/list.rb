14bb63b63190d6ba9ea69266a5a7111ea30edf88
Anthony Gawbill <gaw316@gmail.com>
2018-07-26 16:21:48 -0400

    validations added, styled more.. now show pages will display actors and their bios too

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,5 +3,14 @@ class List < ApplicationRecord
   scope :not_private, -> { where(private: false) }
   serialize :movie_ids
   belongs_to :user
+  validates :name, length: { minimum: 7 }
+  validates :description, length: { minimum: 10 }
+  validate :validate_movie_ids
+
+  def validate_movie_ids
+    if self.movie_ids[0].length<1
+    errors.add(:movie_ids, :invalid)
+  end
+  end
 
 end
