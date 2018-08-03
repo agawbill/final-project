cac25e578862a0a5e00708a78caa62fdad8d3c0c
Anthony Gawbill <gaw316@gmail.com>
2018-08-02 23:25:05 -0400

    final push

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,17 @@ class UsersController < ApplicationController
 
   def show
 
+
+
+
+
+
+
   @user= User.find(params[:id])
   @public=[]
   @private=[]
+  @latest=[]
+  @recent=@user.lists.last
     for movie in @user.lists do
       if movie.private?
       @private.push(movie)
@@ -13,7 +21,16 @@ class UsersController < ApplicationController
       @public.push(movie)
       end
     end
-    
+
+    if @recent.category=="Movies"
+      for movie in @recent.movie_ids[0]
+        @latest.push(Tmdb::Movie.detail(movie))
+      end
+    else
+      for movie in @recent.movie_ids[0]
+        @latest.push(Tmdb::Person.detail(movie))
+      end
+    end
 
 
   end
